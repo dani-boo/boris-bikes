e9be850cd1f06490083b1711223966537edfe908
dani <daniboo83@gmail.com>
2018-07-11 17:23:08 +0100

    Added functionality up to step 11

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,5 +1,19 @@
 class DockingStation
+  attr_reader :bike
+
   def release_bike
     Bike.new
+  end
+
+  def bike
+  end
+  
+  def dock(bike)
+    # return the bike we dock
+    @bike = bike
+  end
+
+  def bike
+    @bike
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,9 +3,18 @@ require 'docking_station'
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
 
-
   it 'releases working bikes' do
     bike = subject.release_bike
     expect(bike).to be_working
   end
+
+  it 'docks something' do
+    bike = Bike.new
+    subject.dock(bike)
+    # return th bike we dock
+    expect(subject.bike).to eq bike
+    # { is_expected.to respond_to(:dock).with(1).argument }
+  end
+  # it { is_expected.to respond_to :bike }
+  
 end
